1273f09a974b4c779a53ea566f8005d56a1b8263
kalhar108 <kalharpatel10@example.com>
2025-02-04 22:31:15 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen AI Project',
+    version= '0.0.0',
+    author= 'Kalhar Patel',
+    author_email= 'kalharpatel9@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
